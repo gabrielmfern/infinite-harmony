21ab83d843384293a6d5cb383d991f6d8c7a6a1c
Gabriel Miranda <gabrielmfern@outlook.com>
2021-08-31 10:33:56 -0300

    - probabbly final version idk

--- video.py
+++ video.py
@@ -27,7 +27,7 @@ class first(Scene):
     def construct(self):
         self.accent_color = '#e1fad2'
 
-        def nth_term(n, l=2):
+        def nth_term(n, l=2.5):
             return Rectangle(l/n, l).set_stroke(self.accent_color, 2)
 
         def h_n(n):
@@ -36,112 +36,157 @@ class first(Scene):
             else:
                 return h_n(n-1) + 1/n
 
-        first_term = Rectangle(3, 3).set_stroke(self.accent_color, 2)
-        terms = VGroup(first_term)
-        self.play(ShowCreation(first_term))
-        self.wait(3)
-        self.play(first_term.animate.scale(2/3))
-        self.play(first_term.animate.to_edge(LEFT))
+        def create_square_divided_into(n_pieces):
+            if n_pieces > 0:
+                pieces = VGroup()
+                term = nth_term(n_pieces)
+                for n in range(n_pieces):
+                    piece = term.copy()
+                    if len(pieces.submobjects) > 0:
+                        piece.next_to(pieces, RIGHT, 0)
+                    pieces.add(piece)
+                pieces.move_to(ORIGIN)
+                return pieces
+        
+        def fade_out_all_pieces_except_first(division):
+            self.play(*(FadeOut(piece) for i, piece in enumerate(division.submobjects) if i > 0))
 
-        first_square = Rectangle(2, 2).set_stroke(
-            self.accent_color, 2).next_to(first_term, UP)
+        square_divided_1 = create_square_divided_into(1)
+        square_divided_1.move_to(UP)
+        squares_divided = [square_divided_1]
+        terms = VGroup(square_divided_1.submobjects[0])
+        self.play(ShowCreation(square_divided_1))
+        self.wait(3)
+        self.play(square_divided_1.submobjects[0].animate.to_corner(LEFT + DOWN))
+        for n in range(2, 9):
+            fade_out_all_pieces_except_first(squares_divided[n - 2])
+            square_divided = create_square_divided_into(n)
+            square_divided.move_to(UP)
+            pieces_amount_brace = Brace(square_divided, UP)
+            pieces_amount_label = Tex(str(n)).next_to(pieces_amount_brace, UP)
+            squares_divided.append(square_divided)
+            self.play(ShowCreation(square_divided), ShowCreation(pieces_amount_brace), Write(pieces_amount_label))
+            self.wait(2/(n**2))
+            self.play(square_divided.submobjects[0].animate.next_to(terms, RIGHT, 0), FadeOut(pieces_amount_brace), FadeOut(pieces_amount_label))
+            terms.add(square_divided.submobjects[0])
+        fade_out_all_pieces_except_first(squares_divided[7])
+        self.wait(2)
+
+        first_term = nth_term(1).to_corner(LEFT + DOWN)
+        new_terms = VGroup(first_term)
+        first_square = Rectangle(2.5, 2.5).set_stroke(self. accent_color, 2).next_to(first_term, UP)
         full_squares = VGroup(first_square)
 
-        trailing_square = Rectangle(0.1, 2)
+        trailing_square = Rectangle(0.00001, 2.5)
         always(trailing_square.next_to, full_squares, RIGHT, 0)
         always(trailing_square.set_stroke, self.accent_color, 2)
-        self.play(ShowCreation(first_square), ShowCreation(trailing_square))
-        for n in range(2, 401):
-            term = nth_term(n).next_to(terms.submobjects[n - 2], RIGHT, 0)
-            terms.add(term)
-            animations = []
-            animations.append(ShowCreation(term))
-            animations.append(
-                trailing_square.animate.set_width(2 * (h_n(n) % 1), True))
+        animations = [ShowCreation(first_square), ShowCreation(trailing_square)]
+        for n in range(2, 33):
+            term = nth_term(n).next_to(new_terms, RIGHT, 0)
+            new_terms.add(term)
+            trailing_square.set_width(2.5 * (h_n(n) % 1), True)
             if math.floor(h_n(n)) - len(full_squares.submobjects) == 1:
-                new_full_square = Rectangle(2, 2).set_stroke(
+                new_full_square = Rectangle(2.5, 2.5).set_stroke(
                     self.accent_color, 2).next_to(full_squares, RIGHT, 0)
                 full_squares.add(new_full_square)
                 animations.append(ShowCreation(new_full_square))
                 trailing_square.next_to(full_squares, RIGHT, 0)
-            self.play(*animations, run_time=8/(n**2))
+                trailing_square.set_width(2.5 * (h_n(n) % 1), False)
+        self.play(*animations, Transform(terms, new_terms))
 
-        self.selected_term_original = None
-        self.selected_term_n = None
-
-        def fade_out_terms_except(n):
-            animations = []
-            for i, term in enumerate(terms):
-                if i != n - 1:
-                    animations.append(FadeOut(term))
-            self.play(*animations, FadeOut(full_squares),
-                      FadeOut(trailing_square), run_time=0.5)
-
-        def fade_in_terms_except(n):
-            animations = []
-            for i, term in enumerate(terms):
-                if i != n - 1:
-                    animations.append(FadeIn(term))
-            self.play(*animations, FadeIn(full_squares),
-                      FadeIn(trailing_square), run_time=0.5)
-
-        def select_specific_term(n):
-            term_to_select = terms.submobjects[n - 1]
-            self.selected_term_original = term_to_select.copy()
-            self.selected_term_n = n
-            fade_out_terms_except(self.selected_term_n)
-            self.play(term_to_select.animate.move_to(ORIGIN))
-            self.play(term_to_select.animate.scale(3/2))
-
-        def get_current_selected_term():
-            return terms[self.selected_term_n - 1]
-
-        def unselect():
-            current_selected_term = get_current_selected_term()
-            self.play(current_selected_term.animate.scale(2/3))
-            self.play(current_selected_term.animate.move_to(
-                self.selected_term_original))
-            fade_in_terms_except(self.selected_term_n)
-            self.selected_term_original = None
-            self.selected_term_n = None
-
-        def make_up_all_pieces(piece, n):
-            self.all_pieces = VGroup()
-            self.all_pieces.add(piece)
-            for pos in range(1, n):
-                new_piece = piece.copy()
-                self.all_pieces.add(new_piece)
-                self.add(new_piece)
-                self.play(new_piece.animate.next_to(
-                    self.all_pieces.submobjects[pos - 1], RIGHT, 0))
-            self.play(self.all_pieces.animate.move_to(ORIGIN))
-
-        def fade_out_pieces_except_for_one():
-            animations = []
-            for i, piece in enumerate(self.all_pieces):
-                if i > 0:
-                    animations.append(FadeOut(piece))
-            self.play(*animations)
-
+        size_demonstration = Brace(terms, UP)
+        size_demonstration_label = Tex('32')
+        size_demonstration_label.next_to(size_demonstration, UP)
+        self.play(Write(size_demonstration), Write(size_demonstration_label))
+        self.wait(8)
+        self.play(FadeOut(size_demonstration), FadeOut(size_demonstration_label))
         self.wait()
-        # Show that the second term is really a half of the square
-        select_specific_term(2)
-        make_up_all_pieces(get_current_selected_term(), 2)
-        self.wait()
-        fade_out_pieces_except_for_one()
-        unselect()
+        self.play(FadeOut(terms), FadeOut(trailing_square), FadeOut(full_squares))
+        giant_text = Text('SÉRIE').scale(10)
+        self.play(Write(giant_text))
+        self.wait(9)
+        self.play(Transform(giant_text, Text('PROVA').scale(9)))
+        self.wait(7)
+        self.play(FadeOut(giant_text), FadeIn(terms))
+        self.play(terms.animate.to_corner(LEFT + UP))
+        
+        self.wait(2)
 
-        # Show that the third term is really a half of the square
-        select_specific_term(3)
-        make_up_all_pieces(get_current_selected_term(), 3)
-        self.wait()
-        fade_out_pieces_except_for_one()
-        unselect()
+        square_divided_2 = create_square_divided_into(2)
+        square_divided_2_amount_brace = always_redraw(Brace, square_divided_2, UP)
+        square_divided_2_amount_label = Tex('2')
+        always(square_divided_2_amount_label.next_to, square_divided_2_amount_brace, UP)
 
-        self.wait()
+        square_divided_4 = create_square_divided_into(4).next_to(square_divided_2, RIGHT)
+        square_divided_4_amount_brace = always_redraw(Brace, square_divided_4, UP)
+        square_divided_4_amount_label = Tex('4')
+        always(square_divided_4_amount_label.next_to, square_divided_4_amount_brace, UP)
+
+        square_divided_8 = create_square_divided_into(8).next_to(square_divided_4, RIGHT)
+        square_divided_8_amount_brace = always_redraw(Brace, square_divided_8, UP)
+        square_divided_8_amount_label = Tex('8')
+        always(square_divided_8_amount_label.next_to, square_divided_8_amount_brace, UP)
+
+        square_divided_16 = create_square_divided_into(16).next_to(square_divided_8, RIGHT)
+        square_divided_16_amount_brace = always_redraw(Brace, square_divided_16, UP)
+        square_divided_16_amount_label = Tex('16')
+        always(square_divided_16_amount_label.next_to, square_divided_16_amount_brace, UP)
+
+        square_powers_2 = VGroup(square_divided_2, square_divided_4, square_divided_8, square_divided_16)
+        square_powers_2_amount_braces = VGroup(square_divided_2_amount_brace, square_divided_4_amount_brace, square_divided_8_amount_brace, square_divided_16_amount_brace)
+        square_powers_2_amount_labels = VGroup(square_divided_2_amount_label, square_divided_4_amount_label, square_divided_8_amount_label, square_divided_16_amount_label)
+        everything_group = VGroup(square_powers_2, square_powers_2_amount_braces, square_powers_2_amount_labels)
+        everything_group.move_to(ORIGIN)
+        everything_group.shift(DOWN)
+        self.play(ShowCreation(everything_group))
+        self.wait(5)
+        def create_group_from_first_half_of(power):
+            index = power - 1
+            result_group = VGroup(
+                *(square_powers_2[index].submobjects[i] for i in range(2**index))
+            )
+            return result_group
+
+        def create_2_times_arrow_from_power_to_next(power):
+            index = power - 1
+            brace_label = square_powers_2_amount_labels[index]
+            next_brace_label = square_powers_2_amount_labels[power]
+            arrow = Arrow(brace_label.get_center(), next_brace_label.get_center())
+            arrow.next_to(brace_label, RIGHT, 0.125)
+            arrow_label = Tex(r'2\times').next_to(arrow, UP)
+            return VGroup(arrow, arrow_label)
+
+        arrow_1 = create_2_times_arrow_from_power_to_next(1)
+        arrow_2 = create_2_times_arrow_from_power_to_next(2)
+        arrow_3 = create_2_times_arrow_from_power_to_next(3)
+        arrows = VGroup(arrow_1, arrow_2, arrow_3)
+        self.play(ShowCreation(arrows))
+
+        self.play(Indicate(create_group_from_first_half_of(1)), Indicate(create_group_from_first_half_of(2)), Indicate(create_group_from_first_half_of(3)), Indicate(create_group_from_first_half_of(4)))
+
+        self.wait(3)
+
+        def create_size_brace_for_first_half_of(power):
+            index = power - 1
+            first_half = create_group_from_first_half_of(power)
+            brace = Brace(first_half, DOWN)
+            brace_label = Tex(str(2 ** index)).next_to(brace, DOWN)
+            return VGroup(brace, brace_label)
+
+        size_1_half = create_size_brace_for_first_half_of(1)
+        size_2_half = create_size_brace_for_first_half_of(2)
+        size_3_half = create_size_brace_for_first_half_of(3)
+        size_4_half = create_size_brace_for_first_half_of(4)
+        sizes = VGroup(size_1_half, size_2_half, size_3_half, size_4_half)
+        self.play(ShowCreation(sizes))
+        self.wait(4)
+        self.play(Indicate(sizes))
+        
+        self.wait(17)
+        self.play(FadeOut(everything_group), FadeOut(sizes), FadeOut(arrows))
 
         sum_inverse_powers_of_2 = PartialSum(
-            lambda n: 1/(2**math.ceil(math.log2(n))), 400, self.accent_color)
+            lambda n: 1/(2**math.ceil(math.log2(n))), 32, self.accent_color, 2.5)
         sum_inverse_powers_of_2.terms.next_to(terms, DOWN, aligned_edge=LEFT)
         power_groups = []
         for i, term in enumerate(sum_inverse_powers_of_2.terms):
@@ -151,14 +196,8 @@ class first(Scene):
                 power_groups[math.ceil(math.log2(i + 1))].add(term)
             self.play(ShowCreation(term), run_time=8/((i+1)**2))
             # self.add(term)
-        self.play(
-            FadeOut(full_squares),
-            FadeOut(trailing_square),
-            sum_inverse_powers_of_2.terms.animate.shift(UP),
-            terms.animate.shift(UP * 2)
-        )
 
-        self.wait()
+        self.wait(4)
 
         def compare_power_group_to_respective_terms(p):
             # Make disappear all the terms that are not in the power group
@@ -182,9 +221,11 @@ class first(Scene):
             self.play(respective_p_group_terms.animate.next_to(power_group, UP))
             comparing_group = VGroup(respective_p_group_terms, power_group)
             self.play(comparing_group.animate.move_to(ORIGIN))
+            animations = []
             for i, term in enumerate(respective_p_group_terms):
-                self.play(power_group[len(
+                animations.append(power_group[len(
                     power_group) - i - 1].animate.next_to(term, DOWN, aligned_edge=LEFT))
+            self.play(*animations)
             self.wait(2)
             animations = []
             for i, term in enumerate(terms):
@@ -205,44 +246,27 @@ class first(Scene):
 
         self.wait()
 
-        compare_power_group_to_respective_terms(1)
-        compare_power_group_to_respective_terms(2)
         compare_power_group_to_respective_terms(3)
         compare_power_group_to_respective_terms(4)
+        compare_power_group_to_respective_terms(5)
 
         self.wait()
-        self.play(Indicate(group_all_power_group_terms(1)), run_time=2)
-        self.wait(2)
         self.play(Indicate(group_all_power_group_terms(2)), run_time=2)
-        self.wait()
         self.play(Indicate(group_all_power_group_terms(3)), run_time=2)
-        self.wait()
         self.play(Indicate(group_all_power_group_terms(4)), run_time=2)
         self.wait()
-        self.play(Indicate(group_all_power_group_terms(5)), run_time=2)
-        self.wait()
-        self.play(Indicate(group_all_power_group_terms(6), run_time=2))
-        self.wait()
-        self.play(Indicate(group_all_power_group_terms(7), run_time=2))
-        self.wait()
 
-        size_demonstration = Brace(terms, DOWN)
-        size_demonstration_label = Tex('400')
-        size_demonstration_label.next_to(size_demonstration, DOWN)
-        self.play(ShowCreation(size_demonstration), Write(size_demonstration_label))
-        self.wait()
-        self.play(FadeOut(size_demonstration), FadeOut(size_demonstration_label))
-        self.wait()
+        self.wait(6)
         harmonic_series = Tex(
             r"\square + \frac{\square}{2} + \frac{\square}{3} + \frac{\square}{4} + \frac{\square}{5} + \frac{\square}{6} + \frac{\square}{7} + \frac{\square}{8} + \cdots")
         harmonic_series.shift(DOWN)
         self.play(FadeOut(sum_inverse_powers_of_2.terms), Write(harmonic_series))
-        self.wait(4)
+        self.wait(6)
         smaller_harmonic_series = Tex(
             r"\square + \frac{\square}{2} + \frac{\square}{4} + \frac{\square}{4} + \frac{\square}{8} + \frac{\square}{8} + \frac{\square}{8} + \frac{\square}{8} + \cdots")
         smaller_harmonic_series.shift(UP)
         self.play(FadeOut(terms), Write(smaller_harmonic_series))
-        self.wait(3)
+        self.wait(4)
         goes_to_infinity = Tex(r"\to\infty")
         goes_to_infinity.next_to(harmonic_series, RIGHT)
         self.play(FadeOut(smaller_harmonic_series), Write(goes_to_infinity))
@@ -251,4 +275,4 @@ class first(Scene):
         new_harmonic_series = Tex(
             r"1 + \frac{1}{2} + \frac{1}{3} + \frac{1}{4} + \frac{1}{5} + \frac{1}{6} + \frac{1}{7} + \frac{1}{8} + \cdots")
         self.play(TransformMatchingTex(harmonic_series, new_harmonic_series))
-        self.wait(2)
+        self.wait(4)
